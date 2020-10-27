e7d44de8e9fbe95ee05c7dcb3fbbbd81d228db95
arkadievvladimir <arkadiev.vladimir@gmail.com>
2020-10-28 02:56:30 +0300

    fix: bag fix

--- covid/script.js
+++ covid/script.js
@@ -103,7 +103,6 @@ var data = {
 }
 
 window.addEventListener('resize', () => {
-    // We execute the same script as before
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
   });
