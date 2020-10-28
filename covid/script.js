a565d50b081a962a00605254dc5b483ac85531cc
arkadievvladimir <arkadiev.vladimir@gmail.com>
2020-10-28 15:14:41 +0300

    feat: add new style

--- covid/script.js
+++ covid/script.js
@@ -1,36 +1,77 @@
 var x = [], 
-    y = [];
+y = [];
+var rem;
+
+function getCovidData() {
 
 
-//var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+  
 var coronaData,
 corona = new XMLHttpRequest(),
 src = 'https://api.covid19api.com/dayone/country/belarus/status/confirmed';
-
 corona.open('GET', src);
 corona.onload = getData;
-//corona.responseType = 'json';
+
 corona.send();
 
-
 function getData () {
-let getData;
-let index;
+let getData,
 
-  coronaData = JSON.parse(this.responseText);
-  
-  for (let i = 1; i < 22; i = i + 1) {
-    index = coronaData.length - i
-    getData = coronaData[index].Date
-    getData = getData.split('-').join('T').split('T')
-    x.push(`${getData[2]}.${getData[1]}`);
-    y.push(coronaData[index].Cases - coronaData[index - 1].Cases);
+index;
+coronaData = JSON.parse(this.responseText);
+
+
+for (let i = 1; i < daysToShowCount + 2; i++) {
+index = coronaData.length - i
+getData = coronaData[index].Date
+getData = getData.split('-').join('T').split('T')
+x.push(`${getData[2]}.${getData[1]}`);
+y.push(coronaData[index].Cases - coronaData[index - 1].Cases);
+}
+
+x = x.reverse();
+y = y.reverse();
+renderGraph(
+  x.filter((el, ind) => ind > x.length - 22), 
+  y.filter((el, ind) => ind > y.length - 22)
+  )
+}
+
+}
+
+var daysToShowCount = 220;
+var showMinData = true;
+getCovidData()
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+
+})
+
+var graph = document.querySelector("#myChart");
+graph.addEventListener('click', () => {
+  if (showMinData) {
+    showMinData = false;
+    data.data.labels = x;
+    data.data.datasets[0].data = y;
+    myChart.update();
+  } else {
+    showMinData = true;
+    data.data.labels = x.filter((el, ind) => ind > x.length - 22);
+    data.data.datasets[0].data = y.filter((el, ind) => ind > y.length - 22);
+    myChart.update();
   }
-  x = x.reverse();
-  y = y.reverse();
+})
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var data = {
+
+var ctx = document.getElementById('myChart').getContext('2d'); 
+var data;
+var myChart = new Chart(ctx, data); 
+function renderGraph(x, y) {
+
+data = {
     type: 'line',
     data: {
         labels: [],
@@ -54,23 +95,27 @@ var data = {
             display: true,
             text: 'Количество новых случаев заражения COVID-19 в день',
             fontStyle: 'bold',
-            fontSize: 15
+            fontSize: 30,
+            padding: 30
         },
         scales: {
+            
             display: false,
             yAxes: [{
                 scaleLabel: {
 					display: false,
 					fontSize: 15,
 					labelString: "Заражений в день"
-				},
+                },
                 ticks: {
-                    beginAtZero: true
+                    beginAtZero: true,
+                    padding: 10
                 },
                 gridLines: {
                     color: 'rgba(0, 0, 0, 0.05)',
                     zeroLineColor: 'rgba(0, 0, 0, 0.5)',
-                    zeroLineWidth: 2
+                    zeroLineWidth: 1.5,
+                    drawTicks: false
                     
                 }
             }],
@@ -80,10 +125,15 @@ var data = {
 					fontSize: 15,
 					labelString: "Дата"
 				},
+        ticks: {
+            beginAtZero: true,
+            padding: 10
+        },
                 gridLines: {
                     color: 'rgba(0, 0, 0, 0.05)',
                     zeroLineColor: 'rgba(0, 0, 0, 0.5)',
-                    zeroLineWidth: 2
+                    zeroLineWidth: 1.5,
+                    drawTicks: false
                 }
             }]
             
@@ -91,18 +141,12 @@ var data = {
 
     }
 };
-  
-  
+Chart.defaults.global.defaultFontSize = 18;
 
-  data.data.labels = x;
-  data.data.datasets[0].data = y;
- 
+data.data.labels = x;
+data.data.datasets[0].data = y;
 
-  var myChart = new Chart(ctx, data); 
-  
+myChart = new Chart(ctx, data); 
+
 }
 
-window.addEventListener('resize', () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  });
